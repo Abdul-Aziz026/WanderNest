272069d4b8f98a06e213c1657a33f5e27e0c8ae4
Abdul_Aziz <57495952+Abdul-Aziz026@users.noreply.github.com>
2024-02-05 13:10:08 +0600

    Update review.js

--- controllers/review.js
+++ controllers/review.js
@@ -1,4 +1,4 @@
-const Review = require("../models/review");
+const Review = require("../models/review"); 
 const Listing = require("../models/listing");
 
 module.exports.createReview = async(req, res) => {
